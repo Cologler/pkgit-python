0308fc4cc1121e09c6c2fbddf0fffb62040cf6da
Cologler <skyoflw@gmail.com>
2019-05-25 21:52:36 +0800

    refactor

--- pkgit/utils/proc_reader.py
+++ pkgit/utils/proc_reader.py
@@ -7,47 +7,18 @@
 
 from threading import Thread, Lock, Condition
 from subprocess import Popen
+from queue import Queue
 
-
-class StreamReader:
-    def __init__(self, stream, cond: Condition):
-        self._stream = stream
-        self._thread: Thread = None
-        self._buffer = []
-        self._end = False
-        self._lock = Lock()
-        self._cond = cond
-
-    def begin_read(self):
-        self._thread = Thread(target=self._run)
-        self._thread.start()
-
-    def _run(self):
-        for line in self._stream:
+def _read_from_stream(stream, queue: Queue, tag):
+    def run():
+        for line in stream:
             line: bytes
-            lines = line.splitlines()
-            with self._lock:
-                self._buffer.extend(lines)
-            with self._cond:
-                self._cond.notify(1)
+            for x in line.splitlines():
+                queue.put((tag, x))
+        queue.put(None)
 
-        with self._lock:
-            self._end = True
-        with self._cond:
-            self._cond.notify(1)
-
-    def end(self):
-        ''' check whether stream is end '''
-        with self._lock:
-            return self._end and not self._buffer
-
-    def get_buffer(self) -> list:
-        with self._lock:
-            try:
-                return self._buffer
-            finally:
-                self._buffer = []
-
+    thread = Thread(target=run)
+    thread.start()
 
 def yield_from_proc(proc: Popen):
     '''
@@ -65,17 +36,14 @@ def yield_from_proc(proc: Popen):
     assert proc.stdout is not None
     assert proc.stderr is not None
 
-    cond = Condition()
-    err_reader = StreamReader(proc.stderr, cond)
-    out_reader = StreamReader(proc.stdout, cond)
-    out_reader.begin_read()
-    err_reader.begin_read()
-    while True:
-        with cond:
-            cond.wait(100)
-        if err_reader.end() and out_reader.end():
-            break
-        if not out_reader.end():
-            yield from [('stdout', l) for l in out_reader.get_buffer()]
-        if not err_reader.end():
-            yield from [('stderr', l) for l in err_reader.get_buffer()]
+    queue = Queue()
+    err_reader = _read_from_stream(proc.stderr, queue, 'stderr')
+    out_reader = _read_from_stream(proc.stdout, queue, 'stdout')
+
+    end_count = 0
+    while end_count < 2:
+        item = queue.get()
+        if item is None:
+            end_count += 1
+        else:
+            yield item
