43f92562791335077a631b14b06711b57b51a91e
Cologler <skyoflw@gmail.com>
2019-07-07 22:33:14 +0800

    ignore reinstall package

--- pkgit/env_builders/pipenv.py
+++ pkgit/env_builders/pipenv.py
@@ -14,25 +14,41 @@ from ..utils.proc_reader import yield_from_proc
 
 declare_requires(Envs.PIPENV, Envs.PYTHON)
 
+PIPFILE_NAME = 'Pipfile'
+
 class PipenvEnvBuilder(IEnvBuilder):
     env = Envs.PIPENV
 
     def fix_env(self):
         if Envs.PIPENV not in self.get_envs():
-            name = 'Pipfile'
-            if self.get_cwd().get_fileinfo(name).is_file():
+            if self.get_cwd().get_fileinfo(PIPFILE_NAME).is_file():
                 self.echo(
                     'added env {} because of found file {}'.format(
                         style(Envs.PIPENV, fg='green'),
-                        style(name, fg='green')
+                        style(PIPFILE_NAME, fg='green')
                     )
                 )
                 self.add_envs(Envs.PIPENV)
 
+    def _list_installed(self, dev: bool) -> list:
+        fileinfo = self.get_cwd().get_fileinfo(PIPFILE_NAME)
+        if fileinfo.is_file():
+            data: dict = fileinfo.load()
+            part: dict = data.get('develop') if dev else data.get('default')
+            if part:
+                return list(part)
+        return []
+
     def _install_from_pipenv(self, package_name: str, **kwargs):
         with self._printer.scoped(self.env):
             args = ['pipenv', 'install', package_name]
-            if kwargs.get('dev', False):
+            dev = kwargs.get('dev', False)
+            if package_name in self._list_installed(dev):
+                return self.echo('Ignore install {} because of it was installed ready.'.format(
+                    style(package_name, fg='green'),
+                ))
+
+            if dev:
                 args.append('--dev')
 
             with self.open_proc(args, stdout=True, stderr=True) as proc:
